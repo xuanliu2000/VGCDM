caecaab6dea98fb26f5b175ed5c9a8220463cc42
Lucian <1633177833@qq.com>
2024-05-14 16:42:42 +0800

    -add requirements

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,4 @@
-from .VQ_QAE.VQ_VAE import VQ_VAE
+from .VQ_VAE.VQ_VAE import VQ_VAE
 from .diffusion.diffusion_1d import GaussianDiffusion1D
 from .diffusion.Unet1D import Unet1D_crossatt,Unet1D
 from .QSCGAN import QSCGAN
